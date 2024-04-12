e3ab9d6f3b57fbb1b70e8d5117dd8c43afb67419
Joko Santosi <ahmadgozali4040@gmail.com>
2024-04-12 16:29:02 +0700

    Update [Creating example.py]

---
+++ example.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+fulltext = ""
+while True:
+    mess = input("Masukan pesan kamu: ")
+    with requests.post("http://127.0.0.1:8000/v1/chat/completions", params={"message": mess}, stream=True) as session:
+        for chunk in session.iter_lines():
+            line = chunk.decode("utf-8")
+            if line == "[DONE]": break
+            else: data = json.loads(line)
+            try:
+                fulltext = data["choices"][0]["message"]["content"]
+            except Exception as e:
+                pass
+    print(fulltext)
